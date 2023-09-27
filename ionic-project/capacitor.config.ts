5360caf9cecd891a336d7c4796806b46e1180e9a
kone mamoudou <konemamoudou84@gmail.com>
2023-09-27 11:28:34 +0200

    :fire: start project

---
+++ ionic-project/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.diginamic',
+  appName: 'ionicProject',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
