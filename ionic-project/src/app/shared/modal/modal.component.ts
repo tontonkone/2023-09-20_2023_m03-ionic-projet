299ebe617b99ae50b37d59e7f16f99ef22e47490
kone mamoudou <konemamoudou84@gmail.com>
2023-09-28 14:14:19 +0200

    test note

---
+++ ionic-project/src/app/shared/modal/modal.component.ts
@@ -0,0 +1,31 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent {
+  
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message = '';
+  name!: string ;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+}
--- ionic-project/src/app/shared/shared/shared.module.ts
+++ ionic-project/src/app/shared/shared/shared.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { ModalComponent } from '../modal/modal.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    ModalComponent
   ],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent,
+  ModalComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FormsModule
+    
   ]
 })
 export class SharedModule { }
