883cb152f0faeccf9880a5bc1d828af644ae1ff6
kone mamoudou <konemamoudou84@gmail.com>
2023-09-28 14:54:38 +0200

    :tada: fin de note

--- ionic-project/src/app/shared/modal/modal.component.ts
+++ ionic-project/src/app/shared/modal/modal.component.ts
@@ -1,31 +1,71 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { Preferences, SetOptions,GetOptions,RemoveOptions, KeysResult } from '@capacitor/preferences';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent {
-  
+export class ModalComponent implements OnInit {
+
   @ViewChild(IonModal) modal!: IonModal;
 
   message = '';
   name!: string ;
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+    if (Capacitor) {
+      const options: GetOptions = {
+        key: 'userNote',
+      };
+
+      Preferences.get(options)
+        .then((result) => {
+          if (result && result.value) {
+            this.name = result.value;
+            this.message = result.value;
+          }
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la récupération des préférences :', error);
+        });
+    }
+  }
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
 
   confirm() {
-    this.modal.dismiss(this.name, 'confirm');
+  
+    if (Capacitor) {
+      console.log(this.name)
+      // Utilisez le module @capacitor/preferences pour stocker la note
+      const options: SetOptions = {
+        key: 'userNote',
+        value: this.name,
+      };
+
+      Preferences.set(options)
+        .then(() => {
+          this.modal.dismiss(this.name, 'confirm');
+        })
+        .catch((error) => {
+          console.error('Erreur lors de l\'enregistrement des préférences :', error);
+        });
+    }
   }
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
+      this.message = `${ev.detail.data}!`;
     }
   }
 }
